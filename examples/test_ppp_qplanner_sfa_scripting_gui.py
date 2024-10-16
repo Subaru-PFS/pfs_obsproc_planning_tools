e2c0b933eff9f5636f788ec14181a05cfe225401
wanqqq31 <wanqiu.he@nao.ac.jp>
2024-10-16 12:48:22 +0900

    Modify GUI

--- examples/test_ppp_qplanner_sfa_scripting_gui.py
+++ examples/test_ppp_qplanner_sfa_scripting_gui.py
@@ -1,8 +1,9 @@
-
 import numpy as np
-from pfs_obsproc_planning import generatePfsDesign, GUI
+from pfs_obsproc_planning import GUI
 
 np.random.seed(1)
 
-gui=GUI.GeneratePfsDesignGUI(repoDir="~/pfs_obsproc_planning_tools/src/pfs_obsproc_planning/")
+gui = GUI.GeneratePfsDesignGUI(
+    repoDir="~/pfs_obsproc_planning_tools/src/pfs_obsproc_planning/"
+)
 gui.run()
